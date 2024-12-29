005096dbdfe7816887019d52fa5561c2d167563c
Georgiy <georgiyryabov2009@gmail.com>
2024-12-29 15:14:42 +0200

    Добавьте файлы проекта.

---
+++ HW 16/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Введіть шлях до вихідного файлу: ");
+            string sourcePath = Console.ReadLine();
+
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("Вихідний файл не знайдено. Перевірте шлях і спробуйте знову.");
+                return;
+            }
+
+            Console.Write("Введіть шлях до файлу, в який потрібно скопіювати дані: ");
+            string destinationPath = Console.ReadLine();
+
+            File.Copy(sourcePath, destinationPath, true);
+
+            Console.WriteLine("Файл успішно скопійовано!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Сталася помилка: {ex.Message}");
+        }
+    }
+}
